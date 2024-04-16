8712aeda00f71c241332fe5b4ee18dd6af2af3aa
Emilio Alejandro Morales Bautista <emilio.morales7916@alumnos.udg.mx>
2024-04-16 15:20:14 -0600

    Rama devE creada

---
+++ MoyoData/Form3.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace MoyoData
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TxtFecha = new System.Windows.Forms.TextBox();
+            this.BtnTrabajador = new System.Windows.Forms.Button();
+            this.TxtProducto = new System.Windows.Forms.TextBox();
+            this.LblProducto = new System.Windows.Forms.Label();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.TxtCantidad = new System.Windows.Forms.TextBox();
+            this.LblCantidad = new System.Windows.Forms.Label();
+            this.LblFechaExpedicion = new System.Windows.Forms.Label();
+            this.LblSProducto = new System.Windows.Forms.Label();
+            this.PnlTitulo = new System.Windows.Forms.Panel();
+            this.PBoxMenu = new System.Windows.Forms.PictureBox();
+            this.PBoxLogo = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.PnlTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.TxtFecha);
+            this.panel1.Controls.Add(this.BtnTrabajador);
+            this.panel1.Controls.Add(this.TxtProducto);
+            this.panel1.Controls.Add(this.LblProducto);
+            this.panel1.Controls.Add(this.TxtUsuario);
+            this.panel1.Controls.Add(this.LblUsuario);
+            this.panel1.Controls.Add(this.TxtCantidad);
+            this.panel1.Controls.Add(this.LblCantidad);
+            this.panel1.Controls.Add(this.LblFechaExpedicion);
+            this.panel1.Controls.Add(this.LblSProducto);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(65, 200);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1339, 514);
+            this.panel1.TabIndex = 4;
+            // 
+            // TxtFecha
+            // 
+            this.TxtFecha.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtFecha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtFecha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.TxtFecha.Location = new System.Drawing.Point(195, 175);
+            this.TxtFecha.Multiline = true;
+            this.TxtFecha.Name = "TxtFecha";
+            this.TxtFecha.Size = new System.Drawing.Size(345, 50);
+            this.TxtFecha.TabIndex = 10;
+            this.TxtFecha.Text = "Ingrese la fecha";
+            // 
+            // BtnTrabajador
+            // 
+            this.BtnTrabajador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(27)))), ((int)(((byte)(20)))));
+            this.BtnTrabajador.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTrabajador.ForeColor = System.Drawing.Color.White;
+            this.BtnTrabajador.Location = new System.Drawing.Point(1096, 428);
+            this.BtnTrabajador.Name = "BtnTrabajador";
+            this.BtnTrabajador.Size = new System.Drawing.Size(173, 53);
+            this.BtnTrabajador.TabIndex = 9;
+            this.BtnTrabajador.Text = "Registrar";
+            this.BtnTrabajador.UseVisualStyleBackColor = false;
+            // 
+            // TxtProducto
+            // 
+            this.TxtProducto.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtProducto.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtProducto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.TxtProducto.Location = new System.Drawing.Point(729, 327);
+            this.TxtProducto.Multiline = true;
+            this.TxtProducto.Name = "TxtProducto";
+            this.TxtProducto.Size = new System.Drawing.Size(345, 50);
+            this.TxtProducto.TabIndex = 8;
+            this.TxtProducto.Text = "Ingrese el producto";
+            // 
+            // LblProducto
+            // 
+            this.LblProducto.AutoSize = true;
+            this.LblProducto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LblProducto.Location = new System.Drawing.Point(724, 287);
+            this.LblProducto.Name = "LblProducto";
+            this.LblProducto.Size = new System.Drawing.Size(114, 29);
+            this.LblProducto.TabIndex = 7;
+            this.LblProducto.Text = "Producto";
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.TxtUsuario.Location = new System.Drawing.Point(729, 175);
+            this.TxtUsuario.Multiline = true;
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(345, 50);
+            this.TxtUsuario.TabIndex = 6;
+            this.TxtUsuario.Text = "Ingrese al usuario";
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LblUsuario.Location = new System.Drawing.Point(724, 128);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(100, 29);
+            this.LblUsuario.TabIndex = 5;
+            this.LblUsuario.Text = "Usuario";
+            // 
+            // TxtCantidad
+            // 
+            this.TxtCantidad.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtCantidad.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtCantidad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.TxtCantidad.Location = new System.Drawing.Point(195, 327);
+            this.TxtCantidad.Multiline = true;
+            this.TxtCantidad.Name = "TxtCantidad";
+            this.TxtCantidad.Size = new System.Drawing.Size(345, 50);
+            this.TxtCantidad.TabIndex = 4;
+            this.TxtCantidad.Text = "Ingrese la Cantidad";
+            // 
+            // LblCantidad
+            // 
+            this.LblCantidad.AutoSize = true;
+            this.LblCantidad.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LblCantidad.Location = new System.Drawing.Point(190, 280);
+            this.LblCantidad.Name = "LblCantidad";
+            this.LblCantidad.Size = new System.Drawing.Size(114, 29);
+            this.LblCantidad.TabIndex = 3;
+            this.LblCantidad.Text = "Cantidad";
+            // 
+            // LblFechaExpedicion
+            // 
+            this.LblFechaExpedicion.AutoSize = true;
+            this.LblFechaExpedicion.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LblFechaExpedicion.Location = new System.Drawing.Point(190, 128);
+            this.LblFechaExpedicion.Name = "LblFechaExpedicion";
+            this.LblFechaExpedicion.Size = new System.Drawing.Size(246, 29);
+            this.LblFechaExpedicion.TabIndex = 1;
+            this.LblFechaExpedicion.Text = "Fecha de expedición";
+            // 
+            // LblSProducto
+            // 
+            this.LblSProducto.AutoSize = true;
+            this.LblSProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSProducto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LblSProducto.Location = new System.Drawing.Point(188, 42);
+            this.LblSProducto.Name = "LblSProducto";
+            this.LblSProducto.Size = new System.Drawing.Size(405, 38);
+            this.LblSProducto.TabIndex = 0;
+            this.LblSProducto.Text = "SALIDA DE PRODUCTO";
+            this.LblSProducto.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // PnlTitulo
+            // 
+            this.PnlTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PnlTitulo.BackColor = System.Drawing.Color.White;
+            this.PnlTitulo.Controls.Add(this.PBoxMenu);
+            this.PnlTitulo.Controls.Add(this.PBoxLogo);
+            this.PnlTitulo.Location = new System.Drawing.Point(65, 41);
+            this.PnlTitulo.Name = "PnlTitulo";
+            this.PnlTitulo.Size = new System.Drawing.Size(1339, 114);
+            this.PnlTitulo.TabIndex = 3;
+            // 
+            // PBoxMenu
+            // 
+            this.PBoxMenu.BackColor = System.Drawing.Color.Transparent;
+            this.PBoxMenu.Image = global::MoyoData.Properties.Resources.menu__1_;
+            this.PBoxMenu.Location = new System.Drawing.Point(49, 24);
+            this.PBoxMenu.Name = "PBoxMenu";
+            this.PBoxMenu.Size = new System.Drawing.Size(56, 56);
+            this.PBoxMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBoxMenu.TabIndex = 1;
+            this.PBoxMenu.TabStop = false;
+            // 
+            // PBoxLogo
+            // 
+            this.PBoxLogo.BackColor = System.Drawing.Color.Transparent;
+            this.PBoxLogo.Image = global::MoyoData.Properties.Resources.Group_3;
+            this.PBoxLogo.Location = new System.Drawing.Point(473, 36);
+            this.PBoxLogo.Name = "PBoxLogo";
+            this.PBoxLogo.Size = new System.Drawing.Size(415, 44);
+            this.PBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PBoxLogo.TabIndex = 0;
+            this.PBoxLogo.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1469, 754);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PnlTitulo);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.PnlTitulo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox TxtFecha;
+        private System.Windows.Forms.Button BtnTrabajador;
+        private System.Windows.Forms.TextBox TxtProducto;
+        private System.Windows.Forms.Label LblProducto;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.Label LblUsuario;
+        private System.Windows.Forms.TextBox TxtCantidad;
+        private System.Windows.Forms.Label LblCantidad;
+        private System.Windows.Forms.Label LblFechaExpedicion;
+        private System.Windows.Forms.Label LblSProducto;
+        private System.Windows.Forms.Panel PnlTitulo;
+        private System.Windows.Forms.PictureBox PBoxMenu;
+        private System.Windows.Forms.PictureBox PBoxLogo;
+    }
+}
