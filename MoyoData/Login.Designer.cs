476d02190b94664433ad21eb06a7edb01e78de45
Carlos Daniel Juarez Castillon <carlos.juarez7917@alumnos.udg.mx>
2024-04-16 19:27:53 -0600

    Cambio de diseño y validación de Login

--- MoyoData/Login.Designer.cs
+++ MoyoData/Login.Designer.cs
@@ -30,12 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.panelIzquierdo = new System.Windows.Forms.Panel();
+            this.LblMensajeError = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.labelLine = new System.Windows.Forms.Label();
             this.LinkLblRecuperarPassword = new System.Windows.Forms.LinkLabel();
             this.BtnIniciarSesion = new System.Windows.Forms.Button();
             this.TbxPassword = new System.Windows.Forms.TextBox();
-            this.TbxCorreo = new System.Windows.Forms.TextBox();
+            this.TbxUsuario = new System.Windows.Forms.TextBox();
             this.PbxNombreEmpresa = new System.Windows.Forms.PictureBox();
             this.BtnCerrar = new System.Windows.Forms.PictureBox();
             this.BtnMinimizar = new System.Windows.Forms.PictureBox();
@@ -50,23 +51,38 @@
             // panelIzquierdo
             // 
             this.panelIzquierdo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
+            this.panelIzquierdo.Controls.Add(this.LblMensajeError);
             this.panelIzquierdo.Controls.Add(this.label1);
             this.panelIzquierdo.Controls.Add(this.labelLine);
             this.panelIzquierdo.Controls.Add(this.LinkLblRecuperarPassword);
             this.panelIzquierdo.Controls.Add(this.BtnIniciarSesion);
             this.panelIzquierdo.Controls.Add(this.TbxPassword);
-            this.panelIzquierdo.Controls.Add(this.TbxCorreo);
+            this.panelIzquierdo.Controls.Add(this.TbxUsuario);
             this.panelIzquierdo.Controls.Add(this.PbxNombreEmpresa);
             this.panelIzquierdo.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelIzquierdo.Location = new System.Drawing.Point(0, 0);
             this.panelIzquierdo.Name = "panelIzquierdo";
             this.panelIzquierdo.Size = new System.Drawing.Size(395, 330);
             this.panelIzquierdo.TabIndex = 1;
+            this.panelIzquierdo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelIzquierdo_MouseDown);
+            // 
+            // LblMensajeError
+            // 
+            this.LblMensajeError.AutoSize = true;
+            this.LblMensajeError.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMensajeError.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.LblMensajeError.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LblMensajeError.Location = new System.Drawing.Point(23, 214);
+            this.LblMensajeError.Name = "LblMensajeError";
+            this.LblMensajeError.Size = new System.Drawing.Size(134, 16);
+            this.LblMensajeError.TabIndex = 7;
+            this.LblMensajeError.Text = "Mensaje de error";
+            this.LblMensajeError.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 184);
+            this.label1.Location = new System.Drawing.Point(51, 178);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(287, 16);
             this.label1.TabIndex = 6;
@@ -75,7 +91,7 @@
             // labelLine
             // 
             this.labelLine.AutoSize = true;
-            this.labelLine.Location = new System.Drawing.Point(51, 127);
+            this.labelLine.Location = new System.Drawing.Point(51, 121);
             this.labelLine.Name = "labelLine";
             this.labelLine.Size = new System.Drawing.Size(287, 16);
             this.labelLine.TabIndex = 5;
@@ -85,12 +101,12 @@
             // 
             this.LinkLblRecuperarPassword.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
             this.LinkLblRecuperarPassword.AutoSize = true;
-            this.LinkLblRecuperarPassword.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LinkLblRecuperarPassword.Font = new System.Drawing.Font("Consolas", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LinkLblRecuperarPassword.LinkColor = System.Drawing.Color.DimGray;
-            this.LinkLblRecuperarPassword.Location = new System.Drawing.Point(109, 304);
+            this.LinkLblRecuperarPassword.Location = new System.Drawing.Point(92, 306);
             this.LinkLblRecuperarPassword.Name = "LinkLblRecuperarPassword";
-            this.LinkLblRecuperarPassword.Size = new System.Drawing.Size(179, 17);
-            this.LinkLblRecuperarPassword.TabIndex = 4;
+            this.LinkLblRecuperarPassword.Size = new System.Drawing.Size(182, 15);
+            this.LinkLblRecuperarPassword.TabIndex = 0;
             this.LinkLblRecuperarPassword.TabStop = true;
             this.LinkLblRecuperarPassword.Text = "¿Olvidaste tu contraseña?";
             // 
@@ -99,43 +115,48 @@
             this.BtnIniciarSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(27)))), ((int)(((byte)(20)))));
             this.BtnIniciarSesion.FlatAppearance.BorderSize = 0;
             this.BtnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIniciarSesion.Font = new System.Drawing.Font("Rosario", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnIniciarSesion.ForeColor = System.Drawing.Color.White;
-            this.BtnIniciarSesion.Location = new System.Drawing.Point(54, 227);
+            this.BtnIniciarSesion.Location = new System.Drawing.Point(85, 242);
             this.BtnIniciarSesion.Name = "BtnIniciarSesion";
-            this.BtnIniciarSesion.Size = new System.Drawing.Size(289, 47);
+            this.BtnIniciarSesion.Size = new System.Drawing.Size(223, 40);
             this.BtnIniciarSesion.TabIndex = 3;
             this.BtnIniciarSesion.Text = "Iniciar sesión";
             this.BtnIniciarSesion.UseVisualStyleBackColor = false;
+            this.BtnIniciarSesion.Click += new System.EventHandler(this.BtnIniciarSesion_Click);
             // 
             // TbxPassword
             // 
             this.TbxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
             this.TbxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.TbxPassword.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxPassword.Font = new System.Drawing.Font("Rosario", 10F);
             this.TbxPassword.ForeColor = System.Drawing.Color.DimGray;
-            this.TbxPassword.Location = new System.Drawing.Point(54, 168);
+            this.TbxPassword.Location = new System.Drawing.Point(54, 161);
             this.TbxPassword.Name = "TbxPassword";
-            this.TbxPassword.Size = new System.Drawing.Size(289, 16);
+            this.TbxPassword.Size = new System.Drawing.Size(289, 21);
             this.TbxPassword.TabIndex = 2;
             this.TbxPassword.Text = "Contraseña";
+            this.TbxPassword.Enter += new System.EventHandler(this.TbxPassword_Enter);
+            this.TbxPassword.Leave += new System.EventHandler(this.TbxPassword_Leave);
             // 
-            // TbxCorreo
+            // TbxUsuario
             // 
-            this.TbxCorreo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
-            this.TbxCorreo.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.TbxCorreo.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TbxCorreo.ForeColor = System.Drawing.Color.DimGray;
-            this.TbxCorreo.Location = new System.Drawing.Point(54, 108);
-            this.TbxCorreo.Name = "TbxCorreo";
-            this.TbxCorreo.Size = new System.Drawing.Size(289, 16);
-            this.TbxCorreo.TabIndex = 1;
-            this.TbxCorreo.Text = "Correo";
+            this.TbxUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
+            this.TbxUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TbxUsuario.Font = new System.Drawing.Font("Rosario", 10F);
+            this.TbxUsuario.ForeColor = System.Drawing.Color.DimGray;
+            this.TbxUsuario.Location = new System.Drawing.Point(54, 104);
+            this.TbxUsuario.Name = "TbxUsuario";
+            this.TbxUsuario.Size = new System.Drawing.Size(289, 21);
+            this.TbxUsuario.TabIndex = 1;
+            this.TbxUsuario.Text = "Usuario";
+            this.TbxUsuario.Enter += new System.EventHandler(this.TbxUsuario_Enter);
+            this.TbxUsuario.Leave += new System.EventHandler(this.TbxUsuario_Leave);
             // 
             // PbxNombreEmpresa
             // 
             this.PbxNombreEmpresa.Image = ((System.Drawing.Image)(resources.GetObject("PbxNombreEmpresa.Image")));
-            this.PbxNombreEmpresa.Location = new System.Drawing.Point(54, 47);
+            this.PbxNombreEmpresa.Location = new System.Drawing.Point(54, 39);
             this.PbxNombreEmpresa.Name = "PbxNombreEmpresa";
             this.PbxNombreEmpresa.Size = new System.Drawing.Size(276, 28);
             this.PbxNombreEmpresa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -151,16 +172,18 @@
             this.BtnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.BtnCerrar.TabIndex = 2;
             this.BtnCerrar.TabStop = false;
+            this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
             // 
             // BtnMinimizar
             // 
             this.BtnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("BtnMinimizar.Image")));
-            this.BtnMinimizar.Location = new System.Drawing.Point(722, 12);
+            this.BtnMinimizar.Location = new System.Drawing.Point(712, 12);
             this.BtnMinimizar.Name = "BtnMinimizar";
             this.BtnMinimizar.Size = new System.Drawing.Size(20, 20);
             this.BtnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.BtnMinimizar.TabIndex = 3;
             this.BtnMinimizar.TabStop = false;
+            this.BtnMinimizar.Click += new System.EventHandler(this.BtnMinimizar_Click);
             // 
             // pictureBoxHormiga1
             // 
@@ -187,6 +210,7 @@
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Login_MouseDown);
             this.panelIzquierdo.ResumeLayout(false);
             this.panelIzquierdo.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PbxNombreEmpresa)).EndInit();
@@ -200,7 +224,7 @@
         #endregion
         private System.Windows.Forms.Panel panelIzquierdo;
         private System.Windows.Forms.PictureBox PbxNombreEmpresa;
-        private System.Windows.Forms.TextBox TbxCorreo;
+        private System.Windows.Forms.TextBox TbxUsuario;
         private System.Windows.Forms.TextBox TbxPassword;
         private System.Windows.Forms.Button BtnIniciarSesion;
         private System.Windows.Forms.LinkLabel LinkLblRecuperarPassword;
@@ -209,5 +233,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelLine;
         private System.Windows.Forms.PictureBox pictureBoxHormiga1;
+        private System.Windows.Forms.Label LblMensajeError;
     }
 }
--- MoyoData/Login.cs
+++ MoyoData/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace MoyoData
 {
@@ -15,6 +16,127 @@ namespace MoyoData
         public Login()
         {
             InitializeComponent();
+        }
+
+        //--------------------------------
+        // Importación para arrastrar
+        // ventana Login
+        //--------------------------------
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg,
+            int wparam, int lparam);
+
+        //--------------------------------
+        // Cerrar ventana
+        //--------------------------------
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //--------------------------------
+        // Minimizar ventana
+        //--------------------------------
+        private void BtnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        //--------------------------------
+        // Arratrar ventana
+        //--------------------------------
+        // En formulario
+        private void Login_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        // En panel
+        private void panelIzquierdo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        //-----------------------------------------------------
+        // Resaltar cuando el puntero entra del
+        // Textbox Usuario
+        //-----------------------------------------------------
+        private void TbxUsuario_Enter(object sender, EventArgs e)
+        {
+            if(TbxUsuario.Text == "Usuario")
+            {
+                TbxUsuario.Text = "";
+                TbxUsuario.ForeColor = Color.Black;
+            }
+        }
+
+        //-----------------------------------------------------
+        // Resaltar cuando el puntero sale del
+        // Textbox Usuario
+        //-----------------------------------------------------
+        private void TbxUsuario_Leave(object sender, EventArgs e)
+        {
+            if (TbxUsuario.Text == "")
+            {
+                TbxUsuario.Text = "Usuario";
+                TbxUsuario.ForeColor= Color.DimGray;
+            }
+        }
+
+        //-----------------------------------------------------
+        // Resaltar cuando el puntero entra del
+        // Textbox Password
+        //-----------------------------------------------------
+        private void TbxPassword_Enter(object sender, EventArgs e)
+        {
+            if(TbxPassword.Text == "Contraseña")
+            {
+                TbxPassword.Text = "";
+                TbxPassword.ForeColor = Color.Black;
+                TbxPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        //-----------------------------------------------------
+        // Resaltar cuando el puntero sale del
+        // Textbox Password
+        //-----------------------------------------------------
+        private void TbxPassword_Leave(object sender, EventArgs e)
+        {
+            if( TbxPassword.Text == "")
+            {
+                TbxPassword.Text = "Contraseña";
+                TbxPassword.ForeColor = Color.DimGray;
+                TbxPassword.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void BtnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            // Validación
+            if(TbxUsuario.Text != "Usuario")
+            {
+                if (TbxPassword.Text != "Contraseña")
+                {
+
+                }
+                else
+                {
+                    MensajeError("Contraseña obligatoria");
+                }
+            }
+            else
+            {
+                MensajeError("Usuario obligatorio");
+            }
+        }
+
+        private void MensajeError (string msg)
+        {
+            LblMensajeError.Text = "    " + msg;
+            LblMensajeError.Visible = true;
         }
     }
 }
